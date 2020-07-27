0bf95a41ef64167d04a815387c626999fa9b846c
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-27 19:56:23 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,7 +13,8 @@ const codes = [
 
 function init() {
   let index =0;
- document.body.addEventListener("keydown", function(e) =>{
-  
+ document.body.addEventListener("keydown", function(e){
+   const key =e.key;
+   
  });
 }
